15a933ef8a4ec8e4b8216edc10eb59b1f62ffcc5
zhangcuoji92 <342055372@qq.com>
2020-10-25 21:39:20 +0800

    two1233654

--- src/main/java/accp/test.java
+++ src/main/java/accp/test.java
@@ -4,5 +4,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("12346");
         System.out.println("22222333");
+        System.out.println("333444");
     }
 }
