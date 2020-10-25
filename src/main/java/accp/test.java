2dbfc0d55c0af9040d8f4c592ef8a71ce1d04942
zhangcuoji92 <342055372@qq.com>
2020-10-25 21:37:11 +0800

    two123

--- src/main/java/accp/test.java
+++ src/main/java/accp/test.java
@@ -3,6 +3,6 @@ package accp;
 public class test {
     public static void main(String[] args) {
         System.out.println("12346");
-        System.out.println("222");
+        System.out.println("22222333");
     }
 }
